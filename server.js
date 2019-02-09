992826670e811f4c1ebe0ec8000df43c92509905
Fer <fgarcia@mail.wm.edu>
2019-02-09 10:59:24 -0500

    dploying to heroku

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(express.static("public"));
 
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
-mongoose.connect(MONGODB_URI);
+mongoose.connect("mongodb://heroku_mtfstjxg:v43nnlse34k0r6flk5tc2s6bi2@ds227185.mlab.com:27185/heroku_mtfstjxg");
 
 
 app.get("/scrape", function (req, res) {
